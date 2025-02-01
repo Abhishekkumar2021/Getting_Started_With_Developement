5baaa7112b024371166535127c00226c78b91029
akansha2026 <akanshaiitkgp2005@gmail.com>
2025-02-01 21:11:35 +0530

    Added delete and add functionality to Todo

--- Temp/todo-manager/src/components/Todo.tsx
+++ Temp/todo-manager/src/components/Todo.tsx
@@ -1,12 +1,17 @@
 import { TodoPropsType } from "../types/todo.types";
 
 export default function Todo(props: TodoPropsType){
-    const {index, todo} = props;
+    const {index, todo , deleteTodo} = props;
+    
+    function handleDelete(){
+        deleteTodo(index)
+    }
 
     return (
         <div className="todo">
             <h1>{todo.title}</h1>
             <h2>{todo.status}</h2>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     )
 }
